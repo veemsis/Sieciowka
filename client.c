43f4729bd383697e46407347c44ef5b18f71f6bf
Karol <cisowski@student.agh.edu.pl>
2016-06-04 23:51:40 +0200

    client multicast function

--- client.c
+++ client.c
@@ -5,10 +5,56 @@
 #include <string.h>
 #include <unistd.h>
 #include <netinet/in.h>
-
+#define EXAMPLE_PORT 6000
+#define EXAMPLE_GROUP "239.0.0.1"
 #define BUFFSIZE 255
 void Die(char *mess) { perror(mess); exit(1); }
+short multicast()
+{
+   struct sockaddr_in addr;
+   int addrlen, sock, cnt;
+   struct ip_mreq mreq;
+   char message[50];
+   bool flaga = false;
+   /* set up socket */
+   sock = socket(AF_INET, SOCK_DGRAM, 0);
+   if (sock < 0) {
+     perror("socket");
+     exit(1);
+   }
+   
+   bzero((char *)&addr, sizeof(addr));
+   addr.sin_family = AF_INET;
+   addr.sin_addr.s_addr = htonl(INADDR_ANY);
+   addr.sin_port = htons(EXAMPLE_PORT);
+   addrlen = sizeof(addr);
+    /* receive */
+      if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {        
+         perror("bind");
+	 exit(1);
+      }    
+      mreq.imr_multiaddr.s_addr = inet_addr(EXAMPLE_GROUP);         
+      mreq.imr_interface.s_addr = htonl(INADDR_ANY);         
+      if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP,
+		     &mreq, sizeof(mreq)) < 0) {
+	 perror("setsockopt mreq");
+	 exit(1);
+      }         
+      while (flaga) {
+ 	 cnt = recvfrom(sock, message, sizeof(message), 0, 
+			(struct sockaddr *) &addr, &addrlen);
+	 if (cnt < 0) {
+	    perror("recvfrom");
+	    exit(1);
+	 } else if (cnt == 0) {
+ 	    break;
+	 }
+	 printf("%s: message = \"%s\"\n", inet_ntoa(addr.sin_addr), message);
+	 flaga=false;
+        }
 
+
+}
 int main(int argc, char *argv[]) {
             int sock;
             struct sockaddr_in echoserver;
